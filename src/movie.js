f07d5efeaff1ffe087058b631f52a14630d335c4
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-18 11:39:15 +0100

    changed

--- src/movie.js
+++ src/movie.js
@@ -4,7 +4,7 @@ let providerHTML = "";
 
 window.onload = function () {
 
-    let renderPage  = document.getElementById('renderPage');
+    // let renderPage  = document.getElementById('renderPage');
     // get id from url
     let selectedId = document.location.search.replace(/^.*?\=/, '');
 
@@ -15,83 +15,82 @@ window.onload = function () {
         const movie = await response.json();
         console.log(movie);
 
-        let movieData = `
-        <header style="background-image: url(https://image.tmdb.org/t/p/original${movie.backdrop_path});background-size:cover;background-position: 0% 20%">
-            <nav class="container">
-                <div class="row">
-                    <div class="col-8">
-                        <img src="./assets/img/movietime_logo.png" class="logo">
-                    </div>
-                    <div class="col-4">
-                        <ul>
-                            <li>
-                                <a href="./index.html"><i class="fas fa-home"></i> Home</a>
-                            </li>
-                            <li>
-                                <a href="./search.html"><i class="fas fa-search"></i> Search</a>
-                            </li>
-                            <li>
-                                <a href="./watchlist.html"><i class="fas fa-plus"></i> Watchlist</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        document.getElementById('movieImage').src = 'https://image.tmdb.org/t/p/w500'+ movie.poster_path;
+        document.getElementById('movieTitle').innerHTML = movie.title
+        document.getElementById('movieYear').innerHTML = movie.id;
 
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-2" id="moviePoster">
-                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-                </div>
-                <div class="col-7">
-                    <h4>${movie.title} <small>${movie.release_date.substring(0, 4)}</small></h4>
-                    <p>${movie.genres[0].name}</p>
-                    <p><i class="far fa-clock"></i> ${movie.runtime}</p>
-                    <small>graph</small>
-                    <h4>Overview</h4>
-                    <p>${movie.overview}</p>
-                </div>
-                <div class="col-3">
-                    BUTTON
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">Details</div>
-                <p>Release Date: ${movie.release_date}</p>
-                <p>Budget $${movie.budget}</p>
-                <p>Revenue: $${movie.revenue}</p>
-            </div>
-        </div>
-        `;
-        renderPage.innerHTML = movieData;
+        // display genres
+        let genresHTML = "";
+        let genres = movie.genres;
+        genres.forEach(genre => {
+            genresHTML += `
+                <p class="genre">${genre.name}</p>
+            `
+        });  
+        document.getElementById("genres").innerHTML = genresHTML;
+
+        document.getElementById('movieRuntime').innerHTML = movie.runtime;
+        document.getElementById('movieOverview').innerHTML = movie.overview;
+
+        document.getElementById('realeaseDate').innerHTML = movie.release_date;
+        document.getElementById('budget').innerHTML = movie.budget;
+        document.getElementById('revenue').innerHTML = movie.revenue;
     }
 
     displayMovie();
 
-    async function getProvider(){   
-        let providerHTML = "";
-
-        const response = await fetch(`https://api.themoviedb.org/3/movie/15/watch/providers?api_key=ace96559b8fb0dd613fdfd48023afa84`);
+    async function getProviders() {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${selectedId}/watch/providers?api_key=ace96559b8fb0dd613fdfd48023afa84`);
         
-        const movieProviders = await response.json();
-        // console.log(movieProviders);
-        const provider = movieProviders.results.BE.buy;
+        
+        const provider = await response.json();
         console.log(provider);
 
-        provider.forEach(p => {
+        let data = provider.results.US.buy;
+        console.log(data);
+
+        let providerHTML = "";
+        data.forEach(p => {
             providerHTML += `
-                <article id="card">
-                    ${p.provider_name}
-                </article>
+            <div class="col-1">
+                <img src="https://image.tmdb.org/t/p/original${p.logo_path}">
+            </div>
             `
-        })
-
+        });
         document.getElementById("providers").innerHTML = providerHTML;
-
     }
 
-    getProvider();
+    getProviders();
+
+
+    // add movie
+    async function addMovie() {
+        console.log('clicked')
+        //Get the data from the form fields
+        let movieId = document.getElementById('movieYear').value;
+        let moviePoster = document.getElementById('movieImage').src.value;
+
+        fetch(`https://web2-backend-rhysdevalckeneer.herokuapp.com/movies/`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                movie_id: movieId,
+                poster_path: moviePoster
+            })
+
+        }).then(response => {
+            return response.json()
+        }).then(async data => {
+            console.log('Success:', data);
+            //Add succes message
+        });
+    }
+    let addButton = document.getElementById('addMovie');
+    addButton.addEventListener("click", addMovie);
 }
+
+
+
 
